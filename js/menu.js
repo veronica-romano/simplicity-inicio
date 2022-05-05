1dc8a6213e1cd088c532e0f6cdf1c9dfe12c49f6
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-05 09:11:38 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,8 @@
 const botaoMenu = document.querySelector(".icone");
-const menu = document.querySelector(".menu")
 
+const menu = document.querySelector(".menu");
+
+botaoMenu.addEventListener('click', function(event){
+    event.preventDefaul();
+    menu.classList.toggle("menu-aberto");
+});
