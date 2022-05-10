fff02e5e23f8bdc14a3fdd67f74ca587d735add6
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-10 08:27:17 -0300

    pagina produtos
    adicionada

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,14 @@
-const botaoMenu = document.querySelector(".icone");
+const botaoMenu = document.querySelector(".icone"); /* nav h2 a */
+const menu = document.querySelector(".menu"); /* nav ul */
 
-const menu = document.querySelector(".menu");
-
-botaoMenu.addEventListener('click', function(event){
-    event.preventDefaul();
+botaoMenu.addEventListener('click', function(event) {
+    event.preventDefault();
     menu.classList.toggle("menu-aberto");
-    if(menu.classList.contains("menu-aberto")){
-            botaoMenu.innerHTML = "Fechar &times;";
-    } else{
+
+    if( menu.classList.contains("menu-aberto") ){
+        botaoMenu.innerHTML = "Fechar &times;";
+    } else {
         botaoMenu.innerHTML = "Menu &equiv;";
     }
+
 });
